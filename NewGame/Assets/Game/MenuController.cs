d051dfcebe6225f9bdd58c3b45d764adb5bfa5f1
NovaCraX <133862867+NovaCraX@users.noreply.github.com>
2024-10-19 11:50:09 +0200

    Update Mario 2.1
    -Add Todo List
    -Add MainMenu option

--- NewGame/Assets/Game/MenuController.cs
+++ NewGame/Assets/Game/MenuController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+//In MenuController wichtig die Level name ändern bei der Richtigen Level die in spiel sein soll!!
+
 public class MenuController : MonoBehaviour
 {
     [Header("Levels To Load")]
     public string _StartGameLevel;
     private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
 
     public void StartGameDialogYes()
     {
         SceneManager.LoadScene(_StartGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
     }
 
     public void ExitButton()
