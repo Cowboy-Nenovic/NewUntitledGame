264719d468bc8531941edc43b8cf0851aeb4b9b4
NovaCraX <133862867+NovaCraX@users.noreply.github.com>
2024-10-19 14:37:24 +0200

    Update 2.2 Mario
    - Addet Gemplay settings (on Work)
    - Addet Audio Settings (current paused)

--- NewGame/Assets/Game/MenuController.cs
+++ NewGame/Assets/Game/MenuController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 //In MenuController wichtig die Level name ändern bei der Richtigen Level die in spiel sein soll!!
 
 public class MenuController : MonoBehaviour
 {
+    //MainMenu
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load")]
     public string _StartGameLevel;
     private string levelToLoad;
@@ -34,5 +45,37 @@ public class MenuController : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    //Audio
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
---
+++ NewGame/Assets/Game/ToDoListe.cs
@@ -0,0 +1,3 @@
+// Main Menu fertig machen      ()
+// Level Devops in Unity ändern von der Test scene zu der richtigen scene       ()
+// ZeggyGames in Credits erwähnen und sich bei ihm melden       ()
